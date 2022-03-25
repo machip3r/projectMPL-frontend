62e38340b21e10cc6bf4c4eff73a81a803ecb0f8
MacHip3r <mac_258@hotmail.com>
2022-03-25 11:20:32 -0600

    project creation

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+//const path = require("path");
+const app = express();
+const port = 3000;
+
+//app.use(express.static(__dirname + "/public"));
+//app.use(express.static(path.join(__dirname, "/public")));
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.route("/", (request, result) => {
+  // result.send("Hello World");
+  result.render("index", {title: "Index Template"});
+});
+
+/* app.route("/test", (request, result) => {
+  result.sendFile(__dirname + "/public/");
+}); */
+
+app.listen(port, () => {
+  console.log("Listening on http://localhost:" + port + "/");
+});
