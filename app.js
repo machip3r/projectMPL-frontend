ba5a5cf4cdb20427999702da53fe8563fdbd2cbd
MacHip3r <mac_258@hotmail.com>
2022-03-29 09:59:39 -0600

    working

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
+//const path = require("path");
 const app = express();
 const port = 3000;
 
-app.route("/", (request, result) => {
-  result.send("Hello World");
+//app.use(express.static(__dirname + "/public"));
+//app.use(express.static(path.join(__dirname, "/public")));
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.get("/", (request, result) => {
+  result.render("index", {title: "Index Template"});
 });
+
+/* app.route("/test", (request, result) => {
+  result.sendFile(__dirname + "/public/");
+}); */
 
 app.listen(port, () => {
   console.log("Listening on http://localhost:" + port + "/");
